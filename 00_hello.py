43ccceab1e9bef9332548da738354ca8cb3145ec
jonyBTT <82463400+JonyBTT@users.noreply.github.com>
2023-02-13 01:26:11 +0100

    Hello.py
    Hola Mundo primer programa de python

---
+++ 00_hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario: 
+# Nuestro hola mundo de Python
+
+"""
+Este es un comentario
+en varias lineas
+
+"""
+print("Hola Python")
+print('Hola Python')
+
+
+'''
+
+Este tambien es un comentario 
+en varias lineas
+
+'''
+
+# Consultar el tipo de dato 
+print(type("Soy un dato str")) # tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(3+ 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'Bool'
