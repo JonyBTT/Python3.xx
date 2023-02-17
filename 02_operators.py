0fce22220b87b3ee1e6feb34f99a124ad7a06fe0
JonyBTT <82463400+JonyBTT@users.noreply.github.com>
2023-02-17 23:25:53 +0100

    Operadores en python

---
+++ 02_operators.py
@@ -0,0 +1,47 @@
+### Operadores Aritmeticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+
+print("Hola " + "python" + "¿Que tal?")
+print ("hola " + str(5))
+print("hola " * 5)
+print("Hola " * (2**3))
+
+my_float = 2.5 * 2
+print("hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola " > "Python ")
+print("hola " < "Python ")
+print("aaaa "  >= "abaa ")  ### ordenacion alfabetica por ASCII ###
+print(len("aaaa ") >= len( "abaa "))  ###  Cuenta caracteres ###
+print("Hola " <="Python ")
+print("Hola " == "Hola ")
+print("Hola " != "Python ")
+
+
+### Operadores Logicos ###
+
+print(3 > 4  and "Hola" > "Python") 
+print(3 > 4  or "Hola" > "Python")
+print(3 < 4  and "Hola" < "Python") 
+print(3 < 4  or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python"  and 4 == 4))
+print(not(3 > 4))
+
+
